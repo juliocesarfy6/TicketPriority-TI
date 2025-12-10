d4d46677dede9430e7f23ff3404fd274be9c2b97
JulioCfy5 <juliocesarfy5@gmail.com>
2025-12-09 22:56:28 -0700

    sidebar, css y un unico detalle con el httpclient pero eso es del back

---
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './sidebar.html',
+  styleUrls: ['./sidebar.css']
+})
+export class SidebarComponent { }
